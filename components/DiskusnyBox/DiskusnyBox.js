bae1474670ac905105cf61d251574560dc036fb6
RubikovaSocka <mincorbr@gmail.com>
2020-07-14 22:50:29 +0200

    discourse

--- components/DiskusnyBox/DiskusnyBox.js
+++ components/DiskusnyBox/DiskusnyBox.js
@@ -19,78 +19,6 @@ export default class DiskusnyBox extends React.Component {
       discourseEmbedUrl: this.props.discourseEmbedUrl
     };
     window.addEventListener("message", this.postMessageReceived, false);
-/*
-    let cssLink = document.createElement("link");
-    cssLink.href = "/styles/iframestyle.css";
-    cssLink.rel = "stylesheet";
-    cssLink.type = "text/css";
-    /*
-    var iframe = document.getElementsByTagName("discourse-embed-frame");
-    iframe.addEventListener("load", function() {
-       the JS equivalent of the answer
-       *
-       * var node = document.createElement('style');
-       * node.appendChild(document.createTextNode('body { background: #fff; }'));
-       * window.frames[0].document.head.appendChild(node);
-       
-
-      // the cleaner and simpler way
-      window.frames[0].document.body.style.backgroundColor = "#d45f44";
-    });*/
-    /*
-    console.log(iframee);
-    var inside = iframee.contentDocument || this.iframe.contentWindow.document;
-    console.log(inside);
-    console.log("before");
-    console.log(inside.head);
-    inside.head.appendChild(cssLink);
-    console.log("after");
-    console.log(inside.head);*/
-/*
-    var iframee = document.getElementById("discourse-embed-frame");
-    var iframe = document.getElementById("discourse-embed-frame");
-    var elmnt = (iframee.contentDocument || iframee.contentWindow.document).getElementsByTagName("html")[0];
-        elmnt.style.display = "none";
-
-/*
-var iframe = document.getElementById('discourse-embed-frame');
-var style = document.createElement('style');
-style.textContent =
-  '.cals {' +
-  '  color: white;' +
-  '}' 
-;
-iframe.contentDocument.head.appendChild(style);
-*/
-/*
-    if (iframee.attachEvent) {
-      iframee.attachEvent("onload", function() {
-        var elmnt = iframe.contentWindow.document.getElementsByTagName("html")[0];
-        elmnt.style.display = "none";
-        /*console.log(iframee);
-        var inside = iframee.contentDocument || iframee.contentWindow.document;
-        inside.head.appendChild(cssLink);
-
-        window.frames[0].document.body.style.backgroundColor = "#d45f44";
-        inside.body.style.backgroundColor = "#d45f45";
-
-        inside.getElementById("body").style.background = "#4db520";*/
-        //document.getElementById('discourse-embed-frame').contentWindow.document.getElementById('header').style.background = "#4db585";
- /*     });
-    } else {
-      iframee.onload = function() {
-        var elmnt = iframe.contentWindow.document.getElementsByTagName("html")[0];
-    elmnt.style.display = "none";
-        /*console.log(iframee);
-        var inside = iframee.contentDocument || iframee.contentWindow.document;
-        inside.head.appendChild(cssLink);
-        window.frames[0].document.body.style.backgroundColor = "#d45f44";
-        inside.body.style.backgroundColor = "#d45f48";
-        inside.getElementById("body").style.background = "#4db521";*/
-    /* };
-    }*/
-
-    //document.getElementById("discourse-embed-frame").html.head.appendChild(cssLink);
   }
 
   componentWillUnmount() {
@@ -140,9 +68,6 @@ iframe.contentDocument.head.appendChild(style);
 
     const iframe = this.iframe;
     const { discourseUrl } = this.props;
-
-    var elmnt = (iframe.contentDocument || iframe.contentWindow.document).getElementsByTagName("html")[0];
-    elmnt.style.display = "none";
 
     if (normalizeUrl(discourseUrl).indexOf(normalizeUrl(e.origin)) === -1) {
       return;
